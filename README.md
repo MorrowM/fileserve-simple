25829812f92c5371670496ca7a2fc33066c2f078
MorrowM <themorrowm@gmail.com>
2021-01-18 21:05:03 +0200

    updated readme, added consts, added message on startup, renamed binary

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # fileserve-simple
 
 ## USAGE:
-    fileserve-simple [OPTIONS] [PORT]
+    fileserve [OPTIONS] [PORT]
 
 ## FLAGS:
     -h, --help       Prints help information
     -V, --version    Prints version information
 
 ## OPTIONS:
-    -b, --bind <ADDRESS>           Alternative bind address [default: 127.0.0.1]
-    -d, --directory <DIRECTORY>    Alternative directory to serve [deafault: curent directory
+    -b, --bind <ADDRESS>           Alternative bind address [default: 0.0.0.0]
+    -d, --directory <DIRECTORY>    Alternative directory to serve [default: curent directory]
     -w, --workers <AMOUNT>         Number of requests that can be handled concurrently [default: 10]
 
 ## ARGS:
--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,28 @@ use clap::{App, Arg};
 use std::net::*;
 use threadpool::ThreadPool;
 
+const DEFAULT_WORKERS: &str = "10";
+const DEFAULT_PORT: &str = "8080";
+const DEFAULT_BIND: &str = "0.0.0.0";
+
 fn main() {
-    let matches = App::new("fileserv-simple")
+    let matches = App::new("fileserve-simple")
         .version("0.1.0")
         .arg(
             Arg::with_name("workers")
                 .short("w")
                 .long("workers")
                 .value_name("AMOUNT")
-                .help("Number of requests that can be handled concurrently [default: 10]")
+                .help(&format!(
+                    "Number of requests that can be handled concurrently [default: {}]",
+                    DEFAULT_WORKERS
+                ))
                 .takes_value(true),
         )
         .arg(
             Arg::with_name("port")
                 .value_name("PORT")
-                .help("Port to run on [default: 8080]")
+                .help(&format!("Port to run on [default: {}]", DEFAULT_PORT))
                 .takes_value(true),
         )
         .arg(
@@ -25,7 +32,10 @@ fn main() {
                 .short("b")
                 .long("bind")
                 .value_name("ADDRESS")
-                .help("Alternative bind address [default: 127.0.0.1]")
+                .help(&format!(
+                    "Alternative bind address [default: {}]",
+                    DEFAULT_BIND
+                ))
                 .takes_value(true),
         )
         .arg(
@@ -33,26 +43,31 @@ fn main() {
                 .short("d")
                 .long("directory")
                 .value_name("DIRECTORY")
-                .help("Alternative directory to serve [deafault: curent directory")
+                .help("Alternative directory to serve [default: curent directory]")
                 .takes_value(true),
         )
         .get_matches();
 
     let n_workers: usize = matches
         .value_of("workers")
-        .unwrap_or("10")
+        .unwrap_or(DEFAULT_WORKERS)
         .parse()
         .expect("Args Error: Invalid worker count");
     let port: u16 = matches
-        .value_of("port")
+        .value_of(DEFAULT_PORT)
         .unwrap_or("8080")
         .parse()
         .expect("Args Error: Invalid port number");
-    let bind = matches.value_of("bind").unwrap_or("127.0.0.1");
+    let bind = matches.value_of("bind").unwrap_or(DEFAULT_BIND);
     let directory = matches.value_of("directory").unwrap_or(".");
 
     let listener = TcpListener::bind((bind, port)).expect("Error: Failed to bind to port");
     let pool = ThreadPool::new(n_workers);
+
+    println!(
+        "Serving HTTP on {} port {} (http://{}:{}/) ...",
+        bind, port, bind, port
+    );
 
     for stream in listener.incoming() {
         if let Ok(mut stream) = stream {
